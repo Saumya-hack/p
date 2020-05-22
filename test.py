cf6fa21d5db4b6700490586eddb26514a7f5d3ed
saumya sharma <saumyanrs99@gmail.com>
2020-05-22 22:24:06 +0530

    second commit

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def func():
-    x=0;
-    i=0;
+    x=0
+    i=0
     while i<20:
-        x=x+2;
-        i+=1;
-    acc=x;
-    return acc;
-f=func();
-print(f);
+        x=x+2
+        i+=1
+    acc=x
+    return acc
+f=func()
+print(f)
     
